034625395a142900c877d9df5f89a12abba34e43
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2019-10-18 16:33:34 +0200

    JVM IR: fix "null" as the first entry in MappedEnumWhenLowering
    If "null" was the first entry in an optimizable "when" over enum,
    mapRuntimeEnumEntry was called before mapConstEnumEntry, and the
    $WhenMappings field was not created. Now both mapConstEnumEntry and
    mapRuntimeEnumEntry create this field on the first access

--- js/js.tests/test/org/jetbrains/kotlin/js/test/semantics/JsCodegenBoxTestGenerated.java
+++ js/js.tests/test/org/jetbrains/kotlin/js/test/semantics/JsCodegenBoxTestGenerated.java
@@ -21297,6 +21297,11 @@ public class JsCodegenBoxTestGenerated extends AbstractJsCodegenBoxTest {
                 runTest("compiler/testData/codegen/box/when/enumOptimization/nonConstantEnum.kt");
             }
 
+            @TestMetadata("nullIsTheFirstEntry.kt")
+            public void testNullIsTheFirstEntry() throws Exception {
+                runTest("compiler/testData/codegen/box/when/enumOptimization/nullIsTheFirstEntry.kt");
+            }
+
             @TestMetadata("nullability.kt")
             public void testNullability() throws Exception {
                 runTest("compiler/testData/codegen/box/when/enumOptimization/nullability.kt");
